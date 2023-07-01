e0a3afa03ae21563b9c58a0fbf3f79d3a94e7999
niall maloney <niall@maloney.ws>
2023-07-01 23:30:24 +0100

    add event id to metadata

--- src/NiallMaloney.EventSourcing/EventMetadata.cs
+++ src/NiallMaloney.EventSourcing/EventMetadata.cs
@@ -2,4 +2,8 @@ using NodaTime;
 
 namespace NiallMaloney.EventSourcing;
 
-public record EventMetadata(Instant OccurredAt, ulong StreamPosition, ulong CategoryStreamPosition);
+public record EventMetadata(
+    string EventId,
+    Instant OccurredAt,
+    ulong StreamPosition,
+    ulong CategoryStreamPosition);
