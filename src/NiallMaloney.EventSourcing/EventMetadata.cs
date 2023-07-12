7b59d2a329876ea7d670bd39372bbf8f559e96cc
niall maloney <niall@maloney.ws>
2023-07-12 22:19:47 +0100

    formatting

--- src/NiallMaloney.EventSourcing/EventMetadata.cs
+++ src/NiallMaloney.EventSourcing/EventMetadata.cs
@@ -7,6 +7,8 @@ namespace NiallMaloney.EventSourcing;
 public record EventMetadata(Uuid EventId, Instant OccurredAt, ulong StreamPosition, ulong AggregatedStreamPosition)
 {
     public static EventMetadata FromResolvedEvent(ResolvedEvent resolvedEvent) =>
-        new(resolvedEvent.Event.EventId, resolvedEvent.Event.Created.ToInstant(), resolvedEvent.Event.EventNumber,
+        new(resolvedEvent.Event.EventId,
+            resolvedEvent.Event.Created.ToInstant(),
+            resolvedEvent.Event.EventNumber,
             resolvedEvent.OriginalEventNumber);
 }
