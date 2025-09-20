76ab76802c9e2e1e5052e0d16f25e0c3ff499426
niall maloney <niall@maloney.ws>
2025-09-20 18:44:18 +0100

    use arm64 container for local testing

--- eventstore-init.sh
+++ eventstore-init.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 docker run --name kurrentdb-node -it -p 2113:2113 \
-    docker.kurrent.io/kurrent-latest/kurrentdb:25.0.1 --insecure --run-projections=All\
+    docker.kurrent.io/kurrent-preview/kurrentdb:25.0.1-experimental-arm64-8.0-jammy \
+    --insecure --run-projections=All \
     --start-standard-projections=true \
     --enable-atom-pub-over-http
